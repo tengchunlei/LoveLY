8d58b37c27f5d78ec8d6c98ed7ac162c9e5934bc
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-09-01 17:20:14 +0800

    完善Footer定位

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from 'styled-components';
 const Footer=styled.footer`
 text-align:center;
-position:absolute;
-bottom:1px;
+position:fixed;
+bottom:0;
 width:100%;
 height:32px;
 background-color:rgb(52, 58, 64);
