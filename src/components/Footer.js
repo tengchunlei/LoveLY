acf0d78b5c6545ee6ad2789680d029a06a8a8e73
tengchunlei <5702225+tengchunlei@user.noreply.gitee.com>
2022-09-08 14:43:54 +0800

    添ædeploy.sh

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,6 @@ import React from 'react'
 import styled from 'styled-components';
 const Footer=styled.footer`
 text-align:center;
-
-bottom:0;
 width:100%;
 height:32px;
 background-color:rgb(52, 58, 64);
